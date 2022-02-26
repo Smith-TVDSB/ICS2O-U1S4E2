c71632cb9c98a0e084192692e73b0a280df86169
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-25 19:37:33 -0500

    fixed print style

--- student_test.py
+++ student_test.py
@@ -4,77 +4,149 @@ import student
 
 
 def test_DirectorKevin():
-    input_values=['Kevin Smith', 'Director']
-    output=[]
+    try:
+        input_values=['Kevin Smith', 'Director']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[2]=='Director Smith'
+        assert output[2]=='Director Smith'
+    except:
+        input_values=['Kevin Smith', 'Director']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[2]+' '+output[3]=='Director Smith'
 
 def test_LlamaJuice():
-    input_values=['Apple Juice', 'Llama']
-    output=[]
+    try:
+        input_values=['Apple Juice', 'Llama']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[2]=='Llama Juice'
+        assert output[2]=='Llama Juice'
+    except:
+        input_values=['Apple Juice', 'Llama']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[2]+' '+output[3]=='Llama Juice'
  
 def test_DoctorStrange():
-    input_values=['Steven Strange', 'Doctor']
-    output=[]
+    try:
+        input_values=['Steven Strange', 'Doctor']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[2]=='Doctor Strange'
+        assert output[2]=='Doctor Strange'
+    except:
+        input_values=['Steven Strange', 'Doctor']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[2]+' '+output[3]=='Doctor Strange'
  
 def test_CaptainKirk():
-    input_values=['Jame Kirk', 'Captain']
-    output=[]
+    try:
+        input_values=['Jame Kirk', 'Captain']
+        output=[]
 
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
 
-    student.main()
+        student.main()
 
-    assert output[2]=='Captain Kirk'
+        assert output[2]=='Captain Kirk'
+    except:
+        input_values=['Jame Kirk', 'Captain']
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        student.input = mock_input
+        student.print = lambda s,t : output.extend([s,t])
+
+        student.main()
+
+        assert output[2]+' '+output[3]=='Captain Kirk'
